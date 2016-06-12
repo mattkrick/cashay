ba292f619735c9acd6f38c438c54bd7d6ad9d9ba
Jordan Husney <jordan.husney@gmail.com>
2016-06-12 13:30:28 -0700

    integrate raw functionality into loader

--- src/schema/cashay-loader.js
+++ src/schema/cashay-loader.js
@@ -1,11 +1,11 @@
 module.exports = function(content) {
   // Allow webpack to be smart about required dependencies:
-  this.cacheable();
+  this.cacheable && this.cacheable();
   const callback = this.async();
   // Execute the supplied javascript, receive promise:
   const doc = this.exec(content, this.resource);
   doc.then(function (schema) {
     // Await the yield of a cashay schema:
-    callback(null, schema);
+    callback(null, "module.exports = " + JSON.stringify(schema));
   });
 };
