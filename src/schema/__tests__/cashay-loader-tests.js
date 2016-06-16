2eda02332335da13a555426e400e9b823b61b71f
Jordan Husney <jordan.husney@gmail.com>
2016-06-16 09:38:19 -0700

    adds backward compatible API; reverts README.md

--- src/schema/__tests__/cashay-loader-tests.js
+++ src/schema/__tests__/cashay-loader-tests.js
@@ -3,19 +3,7 @@ import test from 'ava';
 
 import cashayLoader from '../cashay-loader';
 
-class MockLoaderParent {
-  constructor(loader, testCallback) {
-    this.loader = loader;
-    this.callback = testCallback;
-  }
-  async() { return this.callback; }
-  cacheable() { return; }
-  exec(content, resource) {
-    return () =>
-      new Promise((resolve) => resolve({ querySchema: "test document" }));
-  }
-  load(testContent) { return this.loader(testContent); }
-}
+
 
 test('cashay-loader is function', t => {
   t.plan(1);
@@ -23,8 +11,49 @@ test('cashay-loader is function', t => {
   t.is(typeof cashayLoader, 'function');
 });
 
-test.cb('cashay-loader returns raw module object', t => {
+test.cb('cashay-loader w/o resourceQuery returns raw module object', t => {
   t.plan(2);
+
+  class MockLoaderParent {
+    constructor(loader, testCallback) {
+      this.loader = loader;
+      this.callback = testCallback;
+    }
+    async() { return this.callback; }
+    cacheable() { return; }
+    exec(content, resource) {
+      return new Promise((resolve) => resolve({ querySchema: "test document" }));
+    }
+    load(testContent) { return this.loader(testContent); }
+  }
+
+  const callback = (errors, doc) => {
+    t.is(errors, null);
+    t.regex(doc, /^module.exports = {/);
+    t.end();
+  }
+
+  const loader = new MockLoaderParent(cashayLoader, callback);
+  loader.load();
+});
+
+test.cb('cashay-loader w/o resourceQuery returns raw module object', t => {
+  t.plan(2);
+
+  class MockLoaderParent {
+    constructor(loader, testCallback) {
+      this.loader = loader;
+      this.callback = testCallback;
+      this.resourceQuery = "?thisIsATest";
+    }
+    async() { return this.callback; }
+    cacheable() { return; }
+    exec(content, resource) {
+      return () =>
+        new Promise((resolve) => resolve({ querySchema: "test document" }));
+    }
+    load(testContent) { return this.loader(testContent); }
+  }
 
   const callback = (errors, doc) => {
     t.is(errors, null);
--- src/schema/cashay-loader.js
+++ src/schema/cashay-loader.js
@@ -2,9 +2,16 @@ module.exports = function(content) {
   // Allow webpack to be smart about required dependencies:
   this.cacheable && this.cacheable();
   const callback = this.async();
-  // Execute the supplied javascript, receive promise:
-  const doc = this.exec(content, this.resource);
-  doc(this.resourceQuery).then(function (schema) {
+
+  // Execute the supplied javascript:
+  let doc = this.exec(content, this.resource);
+  if (this.resourceQuery) {
+    // if we've been given a resource query, evaluate the doc as a function
+    // yielding a promise:
+    doc = doc(this.resourceQuery);
+  }
+
+  doc.then(function (schema) {
     // Await the yield of a cashay schema:
     callback(null, "module.exports = " + JSON.stringify(schema));
   });
