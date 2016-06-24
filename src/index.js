27cdadab7d22c8078ff2e80daf7d8a31800006f2
mattkrick <matt.krick@gmail.com>
2016-06-24 16:10:07 -0700

    export ssr constructor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export cashayReducer from './normalize/duck';
 export cashay from './Cashay';
 export HTTPTransport from './transports/HTTPTransport';
+export ServerSideTransport from './transports/ServerSideTransport';
 export transformSchema from './schema/transformSchema';
