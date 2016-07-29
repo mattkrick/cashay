0f16a94a0adb39693334ea926d8a83d0449c910a
mattkrick <matt.krick@gmail.com>
2016-07-29 09:38:21 -0700

    add support for updating a single-document subscription.

--- src/subscribe/getNewDenormalizedData.js
+++ src/subscribe/getNewDenormalizedData.js
@@ -1,90 +1,89 @@
-import getReturnType from './getReturnType';
 import {TypeKind} from 'graphql/type/introspection';
-import mergeStores from '../normalize/mergeStores';
-import {without, ensureTypeFromNonNull} from '../utils';
+import {isObject, ensureTypeFromNonNull, ensureRootType} from '../utils';
 
-const {LIST, OBJECT, SCALAR} = TypeKind;
+const {LIST} = TypeKind;
 
-const getFieldAndKind = (cachedResult, rawFieldName, idFieldName) => {
-  if (rawFieldName.startsWith('[')) {
-    const id = rawFieldName.substr(1, rawFieldName.length - 2);
-    return {
-      field: cachedResult.find(doc => doc[idFieldName] === id),
-      isArray: true
-    }
-  }
-  return {
-    field: cachedResult[rawFieldName],
-    isArray: false
-  }
+const splitPathPiece = (piece) => {
+  const [identifier, rawIndexer] = piece.split('[');
+  const indexer = rawIndexer && rawIndexer.substr(0, rawIndexer.length - 1);
+  return {identifier, indexer};
 };
 
 export default function createNewData(handler, cachedResult, pathArray, context) {
-  const {document, schema, idFieldName} = context;
+  const {document, schema, idFieldName, removeKeys} = context;
   const subSchema = schema.subscriptionSchema;
   const walkPathRecursion = (cachedResult, pathArray, subSchema) => {
-    const rawFieldName = pathArray[0];
-    const {field} = getFieldAndKind(cachedResult, rawFieldName, idFieldName);
-    const fieldSchema = subSchema.fields[rawFieldName];
-    const fieldSchemaType = ensureTypeFromNonNull(fieldSchema.type);
+    const {identifier, indexer} = splitPathPiece(pathArray[0]);
+    const fieldSchemaType = ensureTypeFromNonNull(subSchema.fields[identifier].type);
+    const isList = fieldSchemaType.kind === LIST;
+    if (indexer && !isList) {
+      throw new Error(`Expected an array, but ${fieldSchemaType.name} is an ${fieldSchemaType.kind}. 
+      Do not provide an indexer (eg '[123]') for adding/removing. Array of arrays are not supported`);
+    }
     if (pathArray.length === 1) {
       // if length is 1, we're in the operation object, so we always return an object full of subscription types
       if (handler === 'ADD') {
-        if (fieldSchemaType.kind === LIST) {
-          const returnField = field || [];
-          return {[rawFieldName]: [...returnField, document]};
+        // ignore the indexer, since we will add it to the end, anyways
+        if (isList) {
+          const returnField = cachedResult[identifier] || [];
+          return {[identifier]: [...returnField, document]};
         }
-        return {[rawFieldName]: document};
+        // pointfeed subscription
+        return {[identifier]: document};
       }
       if (handler === 'REMOVE') {
-        if (fieldSchemaType.kind === LIST) {
-          const returnField = field || [];
+        // TODO not sure if any of this handler is correct
+        // ignore the indexer, since we will remove whatever object has the id of "document"
+        if (isList) {
+          const returnField = cachedResult[identifier] || [];
           const idxToRemove = returnField.findIndex(field => field[idFieldName] === document);
-          const newReturnField = returnField.slice();
-          if (idxToRemove !== -1) {
-            newReturnField.splice(idxToRemove, 1);
+          if (idxToRemove === -1) {
+            throw new Error(`Cannot find object with id: ${indexer} in path: ${pathArray} in array: ${returnField}`);
           }
-          return {[rawFieldName]: newReturnField};
+          return {[identifier]: [...returnField.slice(0, idxToRemove), ...returnField.slice(idxToRemove + 1)]};
         }
-        return {[rawFieldName]: undefined};
+        // pointfeed subscription. this will probably never happen, but give en an object so they don't code defensively
+        return isObject(cachedResult[identifier]) ? {} : null;
       }
       if (handler === 'UPDATE') {
-        if (fieldSchemaType.kind === LIST) {
-          // TODO
-        }
-        return {
-          [rawFieldName]: {
-            ...field,
-            ...document
+        if (isList) {
+          if (!indexer) {
+            throw new Error(`Cannot update an array, document ID not specified in path: ${pathArray}`)
+          }
+          const returnField = cachedResult[identifier] || [];
+          const idxToUpdate = returnField.findIndex(field => field[idFieldName] === indexer);
+          if (idxToUpdate === -1) {
+            throw new Error(`Cannot find object with id: ${indexer} in path: ${pathArray} in array: ${returnField}`);
+          }
+          const oldDoc = returnField[idxToUpdate];
+          let updatedDoc;
+          if (isObject(oldDoc)) {
+            updatedDoc = {...oldDoc, ...document};
+            removeKeys.forEach(key => delete updatedDoc[key]);
+          } else {
+            updatedDoc = document;
+          }
+          return {
+            [identifier]: [...returnField.slice(0, idxToUpdate), updatedDoc, ...returnField.slice(idxToUpdate + 1)]
           }
         }
+        const updatedDoc = {...cachedResult[identifier], ...document};
+        removeKeys.forEach(key => delete updatedDoc[key]);
+        return {[identifier]: updatedDoc};
       }
     }
-    const typeSchema = schema.types[fieldSchemaType.name];
+    // TODO THIS IS ALL UNTESTED
+    const rootFieldSchemaType = ensureRootType(fieldSchemaType);
+    const typeSchema = schema.types[rootFieldSchemaType.name];
+    const nextObj = cachedResult[identifier];
+    if (Array.isArray(nextObj) && !indexer) {
+      throw new Error(`${pathArray} does not include an indexer, but ${nextObj} is an array. Include an indexer.`)
+    } else if (!Array.isArray(nextObj) && indexer) {
+      throw new Error(`An indexer was provided: ${indexer} but the object is not an array: ${nextObj}. Don't include the indexer`)
+    }
+    const field = indexer ? nextObj.find(obj => obj.id === indexer) : nextObj;
     const fieldResult = walkPathRecursion(field, pathArray.slice(1), typeSchema);
-    return Array.isArray(field) ? [...field, ...fieldResult] : {...field, ...fieldResult};
+    return {...field, ...fieldResult};
   };
   return walkPathRecursion(cachedResult, pathArray, subSchema);
 };
-
-// export const addDenormalizedData = (cachedResult, pathArray, context) => {
-//   return walkPath(cachedResult, pathArray, context);
-// };
-//
-
-// export const updateDenormalizedData = (typeName, schema, cachedResponse, document, removeKeys, idFieldName) => {
-//   const returnType = getReturnType(typeName, schema);
-//   if (returnType === SCALAR) {
-//     return document;
-//   }
-//   if (returnType === OBJECT) {
-//     const dataToMerge = without(cachedResponse.data, removeKeys);
-//     return mergeStores(dataToMerge, document);
-//   } else if (returnType === LIST) {
-//     const docId = document[idFieldName];
-//     const idxToUpdate = cachedResponse.data.findIndex(doc => doc[idFieldName] === docId);
-//     const dataToMerge = without(cachedResponse.data[idxToUpdate], removeKeys);
-//     const newDoc = mergeStores(dataToMerge, document);
-//     return [...cachedResponse.data.slice(0, idxToUpdate), newDoc, ...cachedResponse.data.slice(idxToUpdate + 1)]
-//   }
-// };
